0947b308ba27870bc7051f3441478c69ea89764e
Rafal <rafal.cwikla@proton.me>
2024-07-23 09:56:54 +0200

    Initialization of courser_project named merklerex

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // tworzymy pętle aby 
+    while (true){
+        // 1 printing help
+        cout << "1: Print help." << endl;
+        // 2 printing exchange stats
+        cout << "2: print exchange stats" << endl;
+        // 3 make an offer
+        cout << "3: Make an offer" << endl;
+        // 4 make a bid
+        cout << "4: Make a bid" << endl;
+        // print wallet
+        cout << "5: Print wallet" << endl;
+        // 6 continue
+        cout << "6: Continue" << endl;
+        // 9 exit
+        cout <<"9: EXIT See you next time" << endl
+             << endl;
+
+        cout << "================================" << endl;
+        cout << "Select option 1 -6" << endl << endl;
+
+        int userOption;
+        cin >> userOption;
+        cout << "You chose: " << userOption << endl << endl;
+
+        if (userOption == 0){
+            cout << "Invalid choose, please select 1-6" << endl << endl;
+        }
+        if (userOption == 1)
+        {
+            cout << "Help - yor=ur aim is to make money. Analyse the market and make bids and offers" << endl
+                << endl;
+        }
+        if (userOption == 2)
+        {
+            cout << "Market looks good" << endl << endl;
+        }
+        if (userOption == 3)
+        {
+            cout << "Mark an offer" << endl
+                << endl;
+        }
+        if (userOption == 4)
+        {
+            cout << "Make a bid - enter an amount" << endl
+                << endl;
+        }
+        if (userOption == 5)
+        {
+            cout << "Your wallet is empty. " << endl
+                << endl;
+        }
+        if (userOption == 6)
+        {
+            cout << "Going to next time frame" << endl
+                << endl;
+        }
+        if (6 > userOption < 9 and userOption >= 10)
+        {
+            cout << "Invalid choose, please select 1-6" << endl
+                << endl;
+        }
+    if (userOption == 9){
+        cout << " See you next time" << endl;
+        break;
+    }
+        
+    }
+    return 0;
+}
