2522777b566614cb9c09f992fd6e4e40690f2737
Rafal <rafal.cwikla@proton.me>
2024-07-25 09:12:32 +0200

    refactroing of klasy.cpp - adding creating vector of objects

--- klasy.cpp
+++ klasy.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 enum class OrderBookType {ask, bid};
 
@@ -9,6 +10,7 @@ enum class OrderBookType {ask, bid};
 class OrderBookEntry{
     public:
     OrderBookEntry(double price, double amount, std::string timeStamp, std::string product, OrderBookType orderType)
+    // Inicjalizacja danych w klasie
         :price(price), amount(amount), timeStamp(timeStamp), product(product), orderType(orderType)
     {
     }
@@ -32,13 +34,32 @@ std::string orderBookTypeToString(OrderBookType type){
 
 int main(){
 
-    OrderBookEntry order1(10000.0, 0.00002, "2020/03/17 17:01:30.099017", "BTC/USDT", OrderBookType::ask);
+    std::vector<OrderBookEntry> orders;
 
-    std::cout << "The price is: " << order1.price << std::endl;
-    std::cout << "The amount is: " << order1.amount << std::endl;
-    std::cout << "The timeStamp is: " << order1.timeStamp << std::endl;
-    std::cout << "The product is: " << order1.product << std::endl;
-    std::cout << "The order type is: " << orderBookTypeToString(order1.orderType) << std::endl;
+    // OrderBookEntry order1(10000.0, 0.00002, "2020/03/17 17:01:30.099017", "BTC/USDT", OrderBookType::ask);
 
+    // Przekazywanie konstruktora klasy do vectora
+    orders.push_back(OrderBookEntry{
+                    10000,
+                    0.02,
+                    "2020/03/17 17:01:30.099017",
+                    "BTC/USDT",
+                    OrderBookType::ask} 
+                );
+
+    orders.push_back(OrderBookEntry{
+                    20000,
+                    0.03,
+                    "2020/03/17 17:21:30.099017",
+                    "BTC/USDT",
+                    OrderBookType::bid} 
+                );
+    std::cout << std::endl;
+    std::cout << "The price is: " << orders[1].price << std::endl;
+    std::cout << "The amount is: " << orders[1].amount << std::endl;
+    std::cout << "The timeStamp is: " << orders[1].timeStamp << std::endl;
+    std::cout << "The product is: " << orders[1].product << std::endl;
+    std::cout << "The order type is: " << orderBookTypeToString(orders[1].orderType) << std::endl;
+    std::cout << std::endl;
     return 0;
 }
